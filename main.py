0f7b6e172c0fe3a8c5ed1976c1e60a485b94c3a7
swela <swelagupta@gmail.com>
2020-09-16 23:43:29 +0530

    Base game

---
+++ main.py
@@ -0,0 +1,162 @@
+from random import randint, choice
+from pprint import pprint, pformat
+import settings
+
+
+class Matrix(object):
+    def __init__(self):
+        self.matrix = self._new_matrix()
+        self.add_new()
+        self.is_end = False
+        self.score = 0
+
+    @property
+    def size(self):
+        return settings.SIZE
+
+    @property
+    def matrix(self):
+        return self._matrix
+
+    @matrix.setter
+    def matrix(self, value):
+        self._matrix = value
+
+    @staticmethod
+    def _new_matrix():
+        return [[0] * settings.SIZE for _ in range(settings.SIZE)]
+
+    def shift(self):
+        new_matrix = self._new_matrix()
+
+        for row in range(settings.SIZE):
+            placer = 0
+
+            for col in range(settings.SIZE):
+
+                if self.matrix[row][col]:
+                    new_matrix[row][placer] = self.matrix[row][col]
+                    placer += 1
+
+        self.matrix = new_matrix
+
+    def reverse(self):
+        new_matrix = self._new_matrix()
+
+        for row in range(settings.SIZE):
+
+            for col in range(settings.SIZE):
+                new_matrix[row][col] = self.matrix[row][settings.SIZE - col - 1]
+
+        self.matrix = new_matrix
+
+    def rotate(self):
+        new_matrix = self._new_matrix()
+
+        for row in range(settings.SIZE):
+
+            for col in range(settings.SIZE):
+                new_matrix[row][col] = self.matrix[col][row]
+
+        self.matrix = new_matrix
+
+    def combine(self):
+        for row in range(settings.SIZE):
+
+            for col in range(settings.SIZE - 1):
+
+                if self.matrix[row][col] and self.matrix[row][col] == self.matrix[row][col + 1]:
+                    self.matrix[row][col] *= 2
+                    self.matrix[row][col + 1] = 0
+                    self.score += self.matrix[row][col]
+
+    def add_new(self):
+        free = [[row, col] for row in range(settings.SIZE) for col in range(settings.SIZE) if not self.matrix[row][col]]
+        if free:
+            new_number = choice([2, 2, 2, 2, 2, 2, 4])
+            new_place = choice(free)
+            self.matrix[new_place[0]][new_place[1]] = new_number
+
+    def check_end(self):
+        if any(2048 in row for row in self.matrix):
+            print("You Win!!!")
+            self.is_end = True
+        if not any(0 in row for row in self.matrix):
+            if not self.valid_move():
+                print("Game over")
+                self.is_end = True
+
+    def valid_move(self):
+        for row in range(settings.SIZE):
+            for col in range(settings.SIZE - 1):
+                if self.matrix[row][col] == self.matrix[row][col + 1]:
+                    return True
+        for row in range(settings.SIZE - 1):
+            for col in range(settings.SIZE):
+                if self.matrix[row][col] == self.matrix[row + 1][col]:
+                    return True
+        return False
+
+    def right(self):
+        self.reverse()
+        self.shift()
+        self.combine()
+        self.shift()
+        self.reverse()
+        self.check_end()
+        if not self.is_end:
+            self.add_new()
+
+    def left(self):
+        self.shift()
+        self.combine()
+        self.shift()
+        self.check_end()
+        if not self.is_end:
+            self.add_new()
+
+    def up(self):
+        self.rotate()
+        self.shift()
+        self.combine()
+        self.shift()
+        self.rotate()
+        self.check_end()
+        if not self.is_end:
+            self.add_new()
+
+    def down(self):
+        self.rotate()
+        self.reverse()
+        self.shift()
+        self.combine()
+        self.shift()
+        self.reverse()
+        self.rotate()
+        self.check_end()
+        if not self.is_end:
+            self.add_new()
+
+    def __repr__(self):
+        return "{}".format(pformat(self.matrix))
+
+
+if __name__ == '__main__':
+    m = Matrix()
+    print(m)
+    counter = 0
+    while not m.is_end:
+        mode = choice(["8", "6", "4", "2"])
+        # mode = input("Enter your move: ")
+        if mode == "8":
+            m.up()
+        elif mode == "2":
+            m.down()
+        elif mode == "4":
+            m.left()
+        else:
+            m.right()
+        counter += 1
+        print("Try: ", counter)
+        print(m)
+        print(m.score)
---
+++ widgets.py
@@ -0,0 +1,105 @@
+import sys
+from PySide2 import QtWidgets, QtCore
+import main
+
+
+class Game(QtWidgets.QWidget):
+    def __init__(self):
+        super(Game, self).__init__()
+        self.setWindowTitle("2048 Game")
+        self.setGeometry(400, 400, 400, 400)
+        self.matrix = main.Matrix()
+        self.setup()
+
+    def setup(self):
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.score = QtWidgets.QLabel("Score: 0")
+        self.score.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.grid = QtWidgets.QGridLayout()
+
+        self.index_holder = [[0] * self.matrix.size for _ in range(self.matrix.size)]
+
+        for i in range(self.matrix.size):
+            for j in range(self.matrix.size):
+                num = Number()
+                self.index_holder[i][j] = num
+                num.setGeometry(50, 50, 50, 50)
+                self.grid.addWidget(num, i, j)
+
+        self.main_layout.addWidget(self.score)
+        self.main_layout.addLayout(self.grid, QtCore.Qt.AlignCenter)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Up:
+            self.up_move()
+        elif event.key() == QtCore.Qt.Key_Down:
+            self.down_move()
+        elif event.key() == QtCore.Qt.Key_Right:
+            self.right_move()
+        elif event.key() == QtCore.Qt.Key_Left:
+            self.left_move()
+        self.refresh()
+        self.score.setText("Score: {}".format(self.matrix.score))
+        event.accept()
+
+    def left_move(self):
+        self.matrix.left()
+
+    def right_move(self):
+        self.matrix.right()
+
+    def up_move(self):
+        self.matrix.up()
+
+    def down_move(self):
+        self.matrix.down()
+
+    def refresh(self):
+        for row in range(self.matrix.size):
+            for col in range(self.matrix.size):
+                self.index_holder[row][col].setText(str(self.matrix.matrix[row][col]))
+
+
+class Number(QtWidgets.QLabel):
+    def __init__(self, number=""):
+        super(Number, self).__init__(number)
+        self.number = number
+        self.setAlignment(QtCore.Qt.AlignCenter)
+        self.setText(self.number)
+
+    def setText(self, text):
+        text = str(text)
+        super(Number, self).setText(text)
+        if text == "0" or text == "":
+            self.setStyleSheet("background-color: #fae0c5")
+        elif text == "2":
+            self.setStyleSheet("background-color: #fab670")
+        elif text == "4":
+            self.setStyleSheet("background-color: #b2ff7a")
+        elif text == "8":
+            self.setStyleSheet("background-color: #ff0000")
+        elif text == "16":
+            self.setStyleSheet("background-color: #7affde")
+        elif text == "32":
+            self.setStyleSheet("background-color: #7abaff")
+        elif text == "64":
+            self.setStyleSheet("background-color: #a56eff")
+        elif text == "128":
+            self.setStyleSheet("background-color: #e264f5")
+        elif text == "256":
+            self.setStyleSheet("background-color: #d13681")
+        elif text == "512":
+            self.setStyleSheet("background-color: #3e2c91")
+        elif text == "1024":
+            self.setStyleSheet("background-color: #2c751b")
+        else:
+            self.setStyleSheet("background-color: #f6ff00")
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    instance = Game()
+    instance.show()
+    app.exec_()
+    sys.exit(0)
